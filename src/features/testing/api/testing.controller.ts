8c2ff902036cd204a2abe9b848aa2c99f2b8d57e
Ivan Kashuba <ikashuba@meninx.net>
2024-03-29 21:14:45 +0200

    Fix delete-all test

--- src/features/testing/api/testing.controller.ts
+++ src/features/testing/api/testing.controller.ts
@@ -1,14 +1,25 @@
 import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { TUserModel, User } from '../../users/domain/user.entity';
+import { Blog, TBlogModel } from '../../blogs/domain/Blog.entity';
+import { Post, TPostModel } from '../../posts/domain/Post.entity';
+import { Comment, TCommentModel } from '../../comments/domain/Comment.entity';
 
 @Controller('testing')
 export class TestingController {
-  constructor(@InjectModel(User.name) private UserModel: TUserModel) {}
+  constructor(
+    @InjectModel(User.name) private UserModel: TUserModel,
+    @InjectModel(Blog.name) private BlogModel: TBlogModel,
+    @InjectModel(Post.name) private PostModel: TPostModel,
+    @InjectModel(Comment.name) private CommentModel: TCommentModel,
+  ) {}
 
   @Delete('all-data')
   @HttpCode(204)
   async deleteUser() {
     await this.UserModel.deleteMany({});
+    await this.BlogModel.deleteMany({});
+    await this.PostModel.deleteMany({});
+    await this.CommentModel.deleteMany({});
   }
 }
